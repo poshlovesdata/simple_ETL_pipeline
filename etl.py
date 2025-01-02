58d040bd34f7866004d2013358c47ec95a883fbc
poshlovesdata <olupelumioshundiya@gmail.com>
2025-01-02 12:01:43 +0100

    Added DOCSTRING comments

--- etl.py
+++ etl.py
@@ -4,11 +4,15 @@ from sqlalchemy import create_engine
 
 
 def extract() -> dict:
+    """
+    This API extracts data from http://universities.hipolabs.com
+    """
     API_URL = "http://universities.hipolabs.com/search?country=United+States"
     data = requests.get(API_URL).json()
     return data
 
 def transform(data:dict) -> pd.DataFrame:
+    """ Transfroms the dataset into desired structure and filters """
     df = pd.DataFrame(data)
     print(f'Total number of Universities from API is {len(data)}')
     df = df[df['name'].str.contains('California')]
@@ -19,6 +23,7 @@ def transform(data:dict) -> pd.DataFrame:
     return df[['domains', 'country', 'web_pages', 'name']]
 
 def load(df:pd.DataFrame) -> None:
+    """ Loads into SQLite database """
     disk_engine = create_engine('sqlite:///my_lite_store.db')
     df.to_sql('cal_uni', disk_engine, if_exists='replace')
     
